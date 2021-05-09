355457da360a54969025ae51e1d120ec58b4a5bc
YevhenSu <susidka.yevhen@gmail.com>
2021-05-09 23:09:07 +0300

    refactoring

--- app.js
+++ app.js
@@ -4,25 +4,25 @@ const year_on = require( "./functions/year_on" );
 const country_data = require( "./functions/country_data" );
 const { COUNTRY, YEAR, url } = require( "./config" );
 
-https.get(url,(res) => {
+https.get( url, ( res ) => {
     let body = "";
 
-    res.on("data", (chunk) => {
+    res.on( "data", ( chunk ) => {
         body += chunk;
-    });
+    } );
 
-    res.on("end", () => {
+    res.on( "end", () => {
         try {
-            const data = JSON.parse(body);
+            const data = JSON.parse( body );
             const obj_years = country_data( COUNTRY, data )[ 0 ];
 			const populationInYear = obj_years[ year_on( YEAR ) ];
 			console.log( populationInYear );        
-		} catch (error) {
-            console.error(error.message);
+		} catch ( error ) {
+            console.error( error.message );
         };
-    });
+    } );
 
-}).on("error", (error) => {
-    console.error(error.message);
-});
-
+} ).on( "error", ( error ) => {
+    console.error( error.message );
+} );
+	
