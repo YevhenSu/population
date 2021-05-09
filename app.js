c270c8cf2545583002472f22121d373b59d27885
YevhenSu <susidka.yevhen@gmail.com>
2021-05-09 17:44:17 +0300

    variable tear_on refactored to function

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const data = require( "./population-figures-by-country-csv_json" );
 const COUNTRY = "Ukraine";
 const YEAR = "2016";
 
-const year_on = "Year_" + YEAR;
+const year_on = year => "Year_" + year;
 
 const country_data = data.filter( x => x["Country"] == COUNTRY );
 //console.log( country_data );
@@ -11,5 +11,5 @@ const country_data = data.filter( x => x["Country"] == COUNTRY );
 const obj_years = country_data[ 0 ];
 //console.log( obj_years );
 
-const populationInYear = obj_years[ year_on ];
+const populationInYear = obj_years[ year_on( YEAR ) ];
 console.log( populationInYear );
