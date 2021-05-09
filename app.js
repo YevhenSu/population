d2521e16df3cd67fcf77cf5c4df77ea4db6e1d02
YevhenSu <susidka.yevhen@gmail.com>
2021-05-09 23:01:01 +0300

    url was moved from app.js to confib.js

--- app.js
+++ app.js
@@ -2,9 +2,7 @@ const https = require('https');
 
 const year_on = require( "./functions/year_on" );
 const country_data = require( "./functions/country_data" );
-const { COUNTRY, YEAR } = require( "./config" );
-
-const url = "https://pkgstore.datahub.io/JohnSnowLabs/population-figures-by-country/population-figures-by-country-csv_json/data/2159fad77778c3b584f3d396593e0af6/population-figures-by-country-csv_json.json";
+const { COUNTRY, YEAR, url } = require( "./config" );
 
 https.get(url,(res) => {
     let body = "";
