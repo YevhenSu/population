503b194c4d1302e33a926f71c39589e3fd1c36ac
YevhenSu <susidka.yevhen@gmail.com>
2021-05-10 16:20:16 +0300

    app formatting spaces was changed

--- app.js
+++ app.js
@@ -12,24 +12,24 @@ let country = ( proc[ 2 ] ) ? proc[ 2 ] : COUNTRY_DEFAULT;
 const year = ( proc[ 3 ] >= 1960 && proc[ 3 ] <= 2016 ) ? proc[ 3 ] : YEAR_DEFAULT;
 
 https.get( url, ( res ) => {
-    let body = "";
+	let body = "";
 
-    res.on( "data", ( chunk ) => {
-        body += chunk;
-    } );
+	res.on( "data", ( chunk ) => {
+		body += chunk;
+	} );
 
-    res.on( "end", () => {
-        try {
-            const data = JSON.parse( body );
+	res.on( "end", () => {
+		try {
+			const data = JSON.parse( body );
 			country = is_country( country, data ) ? country : COUNTRY_DEFAULT;
-            const obj_years = country_data( country, data )[ 0 ];
+			const obj_years = country_data( country, data )[ 0 ];
 			const population_in_year = obj_years[ year_on( year ) ];
 			console.log( `Population in ${ country } in ${ year }: ${ population_in_year }` );        
 		} catch ( error ) {
-            console.error( error.message );
-        };
-    } );
+			console.error( error.message );
+		};
+	} );
 
 } ).on( "error", ( error ) => {
-    console.error( error.message );
+	console.error( error.message );
 } );
