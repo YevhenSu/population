461b92e013ae4296a7e1e986323b922a99363e73
YevhenSu <susidka.yevhen@gmail.com>
2021-05-10 15:55:43 +0300

    input verification was added

--- app.js
+++ app.js
@@ -2,11 +2,14 @@ const process = require('process');
 const https = require( 'https' );
 
 const year_on = require( "./functions/year_on" );
-const country_data = require( "./functions/country_data" );
+const { country_data, is_country } = require( "./functions/country_data" );
 const { COUNTRY_DEFAULT, YEAR_DEFAULT, url } = require( "./config" );
 
-const country = process.argv[ 2 ] ? process.argv[ 2 ] : COUNTRY_DEFAULT;
-const year = process.argv[ 3 ] ? process.argv[ 3 ] : YEAR_DEFAULT;
+const proc = process.argv;
+
+//const country_is =  
+let country = ( proc[ 2 ] ) ? proc[ 2 ] : COUNTRY_DEFAULT;
+const year = ( proc[ 3 ] >= 1960 && proc[ 3 ] <= 2016 ) ? proc[ 3 ] : YEAR_DEFAULT;
 
 https.get( url, ( res ) => {
     let body = "";
@@ -18,6 +21,7 @@ https.get( url, ( res ) => {
     res.on( "end", () => {
         try {
             const data = JSON.parse( body );
+			country = is_country( country, data ) ? country : COUNTRY_DEFAULT;
             const obj_years = country_data( country, data )[ 0 ];
 			const population_in_year = obj_years[ year_on( year ) ];
 			console.log( `Population in ${ country } in ${ year }: ${ population_in_year }` );        
--- functions/country_data.js
+++ functions/country_data.js
@@ -1,1 +1,4 @@
-module.exports = ( country, data ) => data.filter( x => x["Country"] == country );
+const is_country = ( country, data ) => data.some( x => x["Country"] == country );
+const country_data = ( country, data ) => data.filter( x => ( x["Country"] === country ) );
+
+module.exports = { country_data, is_country  };
