42afa49f4670ec58f63d9ee5ddb7c5b7a1dd88d7
YevhenSu <susidka.yevhen@gmail.com>
2021-05-10 16:04:06 +0300

    functions/country_data was refactored

--- functions/country_data.js
+++ functions/country_data.js
@@ -1,4 +1,4 @@
-const is_country = ( country, data ) => data.some( x => x["Country"] == country );
-const country_data = ( country, data ) => data.filter( x => ( x["Country"] === country ) );
+const is_country = ( country, data ) => data.some( obj => obj["Country"] == country );
+const country_data = ( country, data ) => data.filter( obj => ( obj["Country"] === country ) );
 
-module.exports = { country_data, is_country  };
+module.exports = { country_data, is_country };
