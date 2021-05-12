a23c968b077a6eb2232bea2c725114d885e87641
YevhenSu <susidka.yevhen@gmail.com>
2021-05-12 19:25:46 +0300

    code refactored inside try block app.js

--- app.js
+++ app.js
@@ -21,8 +21,10 @@ https.get( url, ( res ) => {
 		try {
 			const data = JSON.parse( body );
 			country = is_country( country, data ) ? country : COUNTRY_DEFAULT;
-			const obj_years = country_data( country, data )[ 0 ];
-			const population_in_year = obj_years[ year_on( year ) ];
+			const obj_years_all = country_data( country, data );
+			const obj_years = obj_years_all[ 0 ];
+			const key = year_on( year );
+			const population_in_year = obj_years[ key ];
 			console.log( `Population in ${ country } in ${ year }: ${ population_in_year }` );        
 		} catch ( error ) {
 			console.error( error.message );
